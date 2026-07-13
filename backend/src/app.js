0a399f330beb9bbef2fe8797233c8575475f8fff
abhishek namdeo <abhisheknamdeo0019@gmail.com>
2026-07-13 22:31:04 +0530

    done

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(
     cors({
         origin: "http://localhost:5173",
-        // origin: "http://localhost:4173",
+       
     
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
